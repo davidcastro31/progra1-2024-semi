09649914916539b36588962d0e401bc9785c8ec6
davidcastro31 <davidguille31@gmail.com>
2024-08-10 15:49:11 -0600

    suma de numeros con sbyte

---
+++ suma_de_numeros_con_sbyte/suma_de_numeros_con_sbyte/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_de_numeros_con_sbyte
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //suma de numeros con sbyt
+            Console.Write("num 1 :");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Console.Write("num2 :");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+
+            Console.WriteLine("La suma de {0} + {1} + {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
