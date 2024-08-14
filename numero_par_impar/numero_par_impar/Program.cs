d3f17d1ebd6aa0dd2a66495965b55db566be3eb5
davidcastro31 <davidguille31@gmail.com>
2024-08-14 10:05:13 -0600

    eterminar si cada numero de la serie es par o impar.

--- numero_par_impar/numero_par_impar/Program.cs
+++ numero_par_impar/numero_par_impar/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace proyecto_fibonacci
+namespace numero_par_impar
 {
     class Program
     {
         static void Main(string[] args){
             //determinar si cada numero de la serie es par o impar.
-            int[] serie = new int[]{ 5,9,4,6,3,2};
-            foreach(int num in serie)
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
             {
                 Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
             }
