057a040d19955a261a0a01ae02f55e05a8f4e35d
davidcastro31 <davidguille31@gmail.com>
2024-09-24 20:40:01 -0600

    Serie de numeros introducidos por el usuario separados por comas, para obtener la media y la desviacion tipica

---
+++ media_aritmetica_datos_de_usuario/media_aritmetica_datos_de_usuario/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_aritmetica_datos_de_usuario
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Write("Ingrese la serie de numeros separados por comas: ");
+            String serie = Console.ReadLine();
+            String[] numeros = serie.Split(',');
+
+            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(numeros), tipica(numeros));
+
+            Console.ReadLine();
+        }
+
+        static double media(string[] serie)
+        {
+            double media = 0;
+            foreach (string num in serie)
+            {
+                media += int.Parse(num);
+            }
+            return media / serie.Length;
+        }
+
+        static double tipica(string[] serie)
+        {
+            double tipica = 0,
+                    m = media(serie);
+            foreach (string num in serie)
+            {
+                tipica += Math.Pow(int.Parse(num) - m, 2);
+            }
+            tipica = Math.Sqrt(tipica / serie.Length);
+            return Math.Round(tipica, 2);
+        }
+    }
+}
